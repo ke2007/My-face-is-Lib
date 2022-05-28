b9c3afee49f2d909249e5122333a28e0e16323ed
dlawn <dlawnsrjs@gmail.com>
2022-05-28 19:42:17 +0900

    📝 : 로 컬 에  있 는  Books.json  데 이 터  파 싱  커 밋
    Resolves(이슈 해결): null
    Ref(참고할 만한 이슈) : null
    Related to(아직 해결 못한 이슈) : S3 버 켓 의  이 미 지  소 스  불 러 오 기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-import "./App.css";
+import { useState } from "react";
+import booksjson from "./Books.json";
+import appstyles from "./App.module.css";
+function App() {
+  const [books, setBooks] = useState(booksjson);
 
-function App() {}
+  console.log(books);
+  return (
+    <div className={appstyles.app}>
+      {books.map((books) => (
+        <div key={books.No}>
+          <h2>"{books.Name}"</h2>
+          <p>지은이 : {books.Writer}</p>
+          <ul>
+            {books.Word.map((W) => (
+              <li key={W}>핵심단어 : {W}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default App;
